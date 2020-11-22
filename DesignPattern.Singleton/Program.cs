35aa2a5ecce0b072c649d4fc1a0da4c0e4ce4682
fzb14 <fzb14@hotmail.com>
2020-11-22 22:10:04 +0800

    Add project files.

---
+++ DesignPattern.Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
