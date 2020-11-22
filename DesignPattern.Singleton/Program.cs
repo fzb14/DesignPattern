19076397d0fd75d4c19f79b392f420a4e5f38f3c
fzb14 <fzb14@hotmail.com>
2020-11-22 23:18:52 +0800

    sloppy singleton

--- DesignPattern.Singleton/Program.cs
+++ DesignPattern.Singleton/Program.cs
@@ -6,7 +6,22 @@ namespace DesignPattern.Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Singleton1 testNew = new Singleton1(); //compile error because the constructor is private.
+            Singleton1 s1 = Singleton1.GetInstance();
+            Singleton1 s2 = Singleton1.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+            s1.someBusinessLogic();
+            Singleton1.staticWork();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DesignPattern.Singleton/Singleton1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    class Singleton1
+    {
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton1() { }
+
+        // The Singleton's instance is stored in a static field. There are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static Singleton1 _instance;
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static Singleton1 GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton1();
+            }
+            return _instance;
+        }
+
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
+        public void someBusinessLogic()
+        {
+            Console.WriteLine("singleton is doing work...");
+        }
+
+        public static void staticWork()
+        {
+            Console.WriteLine("singleton is executing static method...");
+        }
+    }
+}
